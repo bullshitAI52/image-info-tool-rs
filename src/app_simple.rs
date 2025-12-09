0777ba53880e37427dd14717a8a990ea83df3bea
zwm5646 <wearverbertha@gmail.com>
2025-12-09 20:05:50 +0800

    Fix clippy collapsible-if warning and bump to 0.1.8

--- src/app_simple.rs
+++ src/app_simple.rs
@@ -808,13 +808,12 @@ impl eframe::App for ImageToolApp {
                                             .frame(true),
                                     );
 
-                                    if response.lost_focus()
-                                        || ui.input(|i| i.key_pressed(egui::Key::Enter))
+                                    if (response.lost_focus()
+                                        || ui.input(|i| i.key_pressed(egui::Key::Enter)))
+                                        && self.save_editing(&ctx_clone)
                                     {
-                                        if self.save_editing(&ctx_clone) {
-                                            self.status_message =
-                                                format!("已重命名: {}", self.edit_buffer);
-                                        }
+                                        self.status_message =
+                                            format!("已重命名: {}", self.edit_buffer);
                                     }
 
                                     if ui.input(|i| i.key_pressed(egui::Key::Escape)) {
